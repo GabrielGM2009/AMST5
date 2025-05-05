4542d262000c405c864c559c0b810a1ebf78fd2f
GabrielGM <96830268+GabrielGM2009@users.noreply.github.com>
2025-05-04 23:01:42 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using System;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Aluno
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public string Nome { get; set; }
+    public int AnoMatricula { get; set; }
+    public int HorasCurso { get; set; }
+    public int? HorasObrigatorias { get; set; }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+class Program
+{
+    static void Main()
+    {
+        // Criando instâncias
+        EtecT tecnico = new EtecT();
+        EtecA ams = new EtecA();
 
-app.UseRouting();
+        // Adicionando alunos
+        tecnico.AdicionarAluno("Alice", 2023, 120);
+        tecnico.AdicionarAluno("Bruno", 2022, 300);
 
-app.UseAuthorization();
+        ams.AdicionarAluno("Carla", 2024, 130);
+        ams.AdicionarAluno("Diego", 2023, 200);
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+        // Exibindo dados
+        tecnico.ExibirAlunos();
+        ams.ExibirAlunos();
+        ams.ExibirAlunosComObrigatoriedade();
+    }
+}
